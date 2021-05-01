83dc6ad47377e44f44026ba4651f4b64e2be7f2f
zhangjun <5024064@qq.com>
2021-05-01 16:12:17 +0800

    发表评论 http://localhost:8082/article/11 service中添加操作，controller中添加路由映射及控制逻辑 前端页面client/comments.html进行评论展示，common/tale_comment.html用来发表评论 评论迁入文章详情底部

--- src/main/java/com/itheima/service/ICommentService.java
+++ src/main/java/com/itheima/service/ICommentService.java
@@ -13,5 +13,7 @@ public interface ICommentService {
     // 获取文章下的评论
     public PageInfo<Comment> getComments(Integer aid, int page, int count);
 
+    // 用户发表评论
+    public void pushComment(Comment comment);
 }
 
--- src/main/java/com/itheima/service/impl/CommentServiceImpl.java
+++ src/main/java/com/itheima/service/impl/CommentServiceImpl.java
@@ -3,7 +3,9 @@ package com.itheima.service.impl;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.itheima.dao.CommentMapper;
+import com.itheima.dao.StatisticMapper;
 import com.itheima.model.domain.Comment;
+import com.itheima.model.domain.Statistic;
 import com.itheima.service.ICommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +25,8 @@ import java.util.List;
 public class CommentServiceImpl implements ICommentService {
     @Autowired
     private CommentMapper commentMapper;
+    @Autowired
+    private StatisticMapper statisticMapper;
 
     // 根据文章id分页查询评论
     @Override
@@ -33,5 +37,14 @@ public class CommentServiceImpl implements ICommentService {
         return commentInfo;
     }
 
+    // 用户发表评论
+    @Override
+    public void pushComment(Comment comment){
+        commentMapper.pushComment(comment);
+        // 更新文章评论数据量
+        Statistic statistic = statisticMapper.selectStatisticWithArticleId(comment.getArticleId());
+        statistic.setCommentsNum(statistic.getCommentsNum()+1);
+        statisticMapper.updateArticleCommentsWithId(statistic);
+    }
 }
 
